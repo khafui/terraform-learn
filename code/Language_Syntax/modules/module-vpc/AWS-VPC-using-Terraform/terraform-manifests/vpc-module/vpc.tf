804732c5c04a38cec6954554c04aa5a83db5460a
khafui <manianthonio@gmail.com>
2022-10-03 06:09:57 +0000

    working on modules using vpc registry

---
+++ code/Language_Syntax/modules/module-vpc/AWS-VPC-using-Terraform/terraform-manifests/vpc-module/vpc.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.16.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  /* enable_vpn_gateway = true */
+  single_nat_gateway = true
+
+  /* CREATE DATABASE */
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
