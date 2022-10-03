4f4816fca016e817d91673996297ecec86632859
Emmanuel Kafui Anthonio <64821369+khafui@users.noreply.github.com>
2022-10-03 09:25:17 +0000

    Working on vpc module

--- code/Language_Syntax/modules/module-vpc/AWS-VPC-using-Terraform/terraform-manifests/vpc-module/vpc.tf
+++ code/Language_Syntax/modules/module-vpc/AWS-VPC-using-Terraform/terraform-manifests/vpc-module/vpc.tf
@@ -14,6 +14,25 @@ module "vpc" {
   single_nat_gateway = true
 
   /* CREATE DATABASE */
+  create_database_subnet_group = true
+  create_datebase_subnet_route_table = true
+  database_subnets = ["10.0.151.0/24","10.0.152.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  /* Tags */
+  public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    Type = "private-subnets"
+  }
+
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
 
   tags = {
     Terraform = "true"
